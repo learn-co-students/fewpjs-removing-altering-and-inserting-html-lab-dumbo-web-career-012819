2fd8d67ce927f2822ea30a2874e6ec3c678939d4
Krystle Justine <KrystleJustine@gmail.com>
2019-03-11 12:13:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+
 // Write your code here!
+
+document.getElementById("main").remove()
+//removed Main in DOM
+
+var newHeader = document.createElement('h1');
+// creates a new h1 eleement
+
+newHeader.id = 'victory'
+//changes the id for h1 and assigns it to 'victory'
+
+newHeader.innerText = "Krystle is the champion";
+// points node to new header
